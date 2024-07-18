7dc799b2fffc1a71c1bd35110140e3af42f0a9d0
Michael Kalango <michaelkalango@gmail.com>
2024-07-18 22:36:30 +0100

    feat: app text styles

---
+++ lib/ui/common/app_text_styles.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static TextStyle ktsH1 = const TextStyle(
+    fontWeight: FontWeight.w800,
+    fontSize: 48,
+  );
+
+  static TextStyle ktsTableHead = const TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 16,
+  );
+
+  static TextStyle ktsH2 = const TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 30,
+  );
+
+  static TextStyle ktsH3 = const TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 24,
+  );
+
+  static TextStyle ktsH4 = const TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+  );
+
+  static TextStyle ktsLarge = const TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+  );
+
+  static TextStyle ktsSmall = const TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
+  );
+
+  static TextStyle ktsLead = const TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 20,
+  );
+
+  static TextStyle ktsP = const TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+  );
+
+  static TextStyle ktsBlockQuote = const TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    fontStyle: FontStyle.italic,
+  );
+
+  static TextStyle ktsSubtitle = const TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+  );
+}
