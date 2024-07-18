96a84c1d5a4c582c6a1300f5bca49a27190ef58f
Michael Kalango <michaelkalango@gmail.com>
2024-07-18 21:55:35 +0100

    feat: app colors

--- lib/ui/common/app_colors.dart
+++ lib/ui/common/app_colors.dart
@@ -7,3 +7,16 @@ const Color kcMediumGrey = Color(0xFF474A54);
 const Color kcLightGrey = Color.fromARGB(255, 187, 187, 187);
 const Color kcVeryLightGrey = Color(0xFFE3E3E3);
 const Color kcBackgroundColor = kcDarkGreyColor;
+
+class AppColors {
+  static Color kcSlate50 = const Color(0xffF8FAFC);
+  static Color kcSlate100 = const Color(0xffF1F5F9);
+  static Color kcSlate200 = const Color(0xffE2E8F0);
+  static Color kcSlate300 = const Color(0xffCBD5E1);
+  static Color kcSlate400 = const Color(0xff94A3B8);
+  static Color kcSlate500 = const Color(0xff64748B);
+  static Color kcSlate600 = const Color(0xff475569);
+  static Color kcSlate700 = const Color(0xff334155);
+  static Color kcSlate800 = const Color(0xff1E293B);
+  static Color kcSlate900 = const Color(0xff0F172A);
+}
